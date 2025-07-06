e499c89862491228fda3b970a02422174a535be7
tkdalove <sumate4454@gmail.com>
2025-07-06 08:15:32 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,370 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "หน้าบ้าน",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.058632079333156284,
+          "pitch": 0.05390870205705234,
+          "rotation": 0,
+          "target": "1-"
+        },
+        {
+          "yaw": 0.30565354077571527,
+          "pitch": 0.11809481890628071,
+          "rotation": 0.7853981633974483,
+          "target": "7-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-",
+      "name": "ห้องนั่งเล่น",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.12697703133359184,
+          "pitch": 0.036777822489831635,
+          "rotation": 0,
+          "target": "6--2"
+        },
+        {
+          "yaw": -0.10402878508705982,
+          "pitch": 0.23595868520222396,
+          "rotation": 1.5707963267948966,
+          "target": "5--2"
+        },
+        {
+          "yaw": -0.2997729383382115,
+          "pitch": 0.28620569758976977,
+          "rotation": 5.497787143782138,
+          "target": "0-"
+        },
+        {
+          "yaw": -2.112473649519563,
+          "pitch": 0.3642069944064126,
+          "rotation": 0,
+          "target": "7-"
+        },
+        {
+          "yaw": -3.0793984201437237,
+          "pitch": 0.2043602802718283,
+          "rotation": 0,
+          "target": "3--1"
+        },
+        {
+          "yaw": 2.577759848804938,
+          "pitch": 0.2586507338885671,
+          "rotation": 0.7853981633974483,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-",
+      "name": "ครัว",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.2450408814688974,
+          "pitch": 0.2166054304754912,
+          "rotation": 0,
+          "target": "1-"
+        },
+        {
+          "yaw": -1.7810227613672822,
+          "pitch": 0.3565808151047172,
+          "rotation": 0,
+          "target": "8-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3--1",
+      "name": "ห้องนอน 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.21960570477241603,
+          "pitch": 0.480445359039944,
+          "rotation": 5.497787143782138,
+          "target": "7-"
+        },
+        {
+          "yaw": 0.3385670283042419,
+          "pitch": 0.25825814828427873,
+          "rotation": 4.71238898038469,
+          "target": "1-"
+        },
+        {
+          "yaw": 0.7023496037073897,
+          "pitch": 0.2719354825202078,
+          "rotation": 1.5707963267948966,
+          "target": "4--1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4--1",
+      "name": "ห้องน้ำ 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.4526050485585618,
+          "pitch": 0.2779693904523093,
+          "rotation": 0,
+          "target": "3--1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5--2",
+      "name": "ห้องน้ำ 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5246042363967014,
+          "pitch": 0.31997203006757147,
+          "rotation": 0,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6--2",
+      "name": "ห้องนอน 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.2329269759931591,
+          "pitch": 0.25906610591810697,
+          "rotation": 0,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-",
+      "name": "พื้นที่สระว่ายน้ำ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5571196399268903,
+          "pitch": 0.190690980391782,
+          "rotation": 0,
+          "target": "1-"
+        },
+        {
+          "yaw": -0.06260369007873656,
+          "pitch": 0.18004890707952015,
+          "rotation": 0,
+          "target": "3--1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-",
+      "name": "หลังบ้าน",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "ATEKA TYPE A",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
